81d24f4258734ebd7aced143c314ae2376c059cf
Hatzer <juanpfederico@gmail.com>
2019-08-03 17:26:58 -0300

    Agrego estilos de bootstrap y font awesome, optimizo plugin

--- wp-content/plugins/github-finder/index.php
+++ wp-content/plugins/github-finder/index.php
@@ -9,30 +9,56 @@
 		Author URI: http://www.juanfederico.com.ar
 	*/
 
+	//Agrego estilos (en este caso de bootstrap) para utilizarlos en los resultados
+	add_action('wp_enqueue_scripts','register_my_scripts');
+
+	function register_my_scripts(){
+	    wp_enqueue_style( 'bootstrap-css1', plugins_url( '/css/bootstrap.css' , __FILE__ ) );
+	    wp_enqueue_style( 'bootstrap-css2', plugins_url( '/css/bootstrap-theme.css' , __FILE__ ) );
+	    wp_enqueue_style( 'bootstrap-css3', plugins_url( '/css/bootstrap-glyphicons.css' , __FILE__ ) );
+	    wp_enqueue_style( 'bootstrap-css4', plugins_url( '/css/bootstrap-grid.css' , __FILE__ ) );
+	    wp_enqueue_style( 'fonts', plugins_url( '/webfonts/fa-solid-900.woff' , __FILE__ ) );
+	    wp_enqueue_style( 'fonts', plugins_url( '/webfonts/fa-solid-900.woff2' , __FILE__ ) );
+	    wp_enqueue_style( 'fa', plugins_url( '/css/all.css' , __FILE__ ) );
+	}
+
 	function githubFinder($atts){
 
 		$dataSalida = "<br>";
-		//Variables que definen el criterio de busqueda de github
-		$query = get_field('query');
-		$ordenamiento = get_field('ordenamiento')['value']; //Valor del select
+		//Variables que definen el criterio de busqueda de GitHub
+		$query = get_field('query'); //Valor del string para la propiedad 'q'
+		$ordenamiento = get_field('ordenamiento')['value']; //Valor del select para propiedad 'sort'
 
 		$url = "https://api.github.com/search/repositories?q=".$query."&sort=".$ordenamiento."&order=desc";
 
-    	$result = wp_remote_get( $url );
-    	$result = json_decode($result['body'], true);
+    	$result = wp_remote_get( $url ); //Consumiendo la API mediante GET
+    	$result = json_decode($result['body'], true); //Transformando la data a lo que nos interesa
 
-		foreach (array_slice($result['items'], 0, 5) as $items){ //Primeros 5 elementos
-	    	$dataSalida .= "<b>Nombre del repositorio: </b>" . $items["name"] . "<br>";
-	    	$dataSalida .= "<b>Cantidad de estrellas: </b>" . $items["stargazers_count"] . "<br>";
-	    	$dataSalida .= "<b>Link al repositorio: </b> 
+		foreach (array_slice($result['items'], 0, 5) as $items){ //Mostrando primeros 5 elementos
+			$dataSalida .= '<div class="panel panel-default">';
+	    	$dataSalida .= '<div class="panel-body">';
+	    	$dataSalida .= '<ul>';
+
+	    	$dataSalida .= '<li>';
+	    	$dataSalida .= "<u>Nombre del repositorio:</u> " . $items["name"] . "<br>";
+	    	$dataSalida .= '</li>';
+
+	    	$dataSalida .= '<li>';
+	    	$dataSalida .= "<u>Cantidad de estrellas:</u> <i class='fa fa-star'></i>" . $items["stargazers_count"] . "<br>";
+	    	$dataSalida .= '</li>';
+
+	    	$dataSalida .= '<li>';
+	    	$dataSalida .= "<u>Link al repositorio:</u>  
 	    		<a href='".$items['html_url']."'>" . $items["html_url"] . "</a><br>";
-	    	$dataSalida .= "<br><br>";
+	    	$dataSalida .= '</li>';
+	    	
+	    	$dataSalida .= '</ul>';
+	    	$dataSalida .= "</div></div>";
+	    	//$dataSalida .= "<br>";
 	    }
 
     	return $dataSalida;
 	}
-
-	
 
 	add_shortcode("github-finder", "githubFinder");
 
