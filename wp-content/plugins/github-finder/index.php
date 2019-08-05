ac9a963b5022b0aa74ba37653fc6868f952d428a
Hatzer <juanpfederico@gmail.com>
2019-08-04 22:58:30 -0300

    Refinando detalles del plugin y testeando su instalacion

--- wp-content/plugins/github-finder/index.php
+++ wp-content/plugins/github-finder/index.php
@@ -2,15 +2,17 @@
 	/*
 		Plugin Name: Buscador github
 		Plugin URI: http://www.google.com.ar
-		Description: Shortcode utilizado para la busqueda de repositorios en github
+		Description: Plugin utilizado para la busqueda de repositorios en github, según un tipo de criterio determinado (acceder a wpnombre/criterios para ver todos)
 		Author: Juan Federico
 		Version: 1.0.0
 		Author URI: http://www.juanfederico.com.ar
 	*/
 
-	//Agrego estilos (en este caso de bootstrap y font-awesome) para utilizarlos en los resultados
-	add_action('wp_enqueue_scripts','register_my_scripts');
+	//Configurando estructura de los enlaces permanentes para que el plugin use la ruta nombrewp/criterios
+	if(strcmp(get_option('permalink_structure')!='/%category%/%post_id%')
+		update_option('permalink_structure', '/%category%/%post_id%');
 
+	//Agrego estilos (en este caso de bootstrap y font-awesome) para utilizarlos en los resultados
 	function register_my_scripts(){
 	    wp_enqueue_style( 'bootstrap-css1', plugins_url( '/css/bootstrap.css' , __FILE__ ) );
 	    wp_enqueue_style( 'bootstrap-css2', plugins_url( '/css/bootstrap-theme.css' , __FILE__ ) );
@@ -19,11 +21,12 @@
 	    wp_enqueue_style( 'fa', plugins_url( '/css/all.css' , __FILE__ ) );
 	    wp_enqueue_style( 'fonts1', plugins_url( '/webfonts/fa-solid-900.woff' , __FILE__ ) );
 	    wp_enqueue_style( 'fonts2', plugins_url( '/webfonts/fa-solid-900.woff2' , __FILE__ ) );
-	    wp_enqueue_script( 'jquery-min', plugins_url( '/js/jquery.min.js' , __FILE__ ) );
-		wp_enqueue_script( 'cb-min', plugins_url( '/dist/clipboard.min.js' , __FILE__ ) );
+	    wp_enqueue_script( 'jquerymin', plugins_url( '/js/jquery.min.js' , __FILE__ ) );
+	    wp_enqueue_script( 'bsmin', plugins_url( '/js/bootstrap.js' , __FILE__ ) );
+		wp_enqueue_script( 'cbmin', plugins_url( '/dist/clipboard.min.js' , __FILE__ ) );
 		wp_enqueue_script( 'funcionalidad', plugins_url( '/js/script.js' , __FILE__ ) );
-	    //wp_enqueue_script( 'clipboard', plugins_url( '/js/script.js' , __FILE__ ) );
 	}
+	add_action('wp_enqueue_scripts','register_my_scripts');
 
 	function githubFinder($atts){
 
@@ -33,13 +36,14 @@
 		$ordenamiento = get_field('ordenamiento')['value']; //Valor del select para propiedad 'sort'
 
 		$url = "https://api.github.com/search/repositories?q=".$query."&sort=".$ordenamiento."&order=desc";
+		$urlClipboard = "https://github.com/search?q=".$query."&sort=".$ordenamiento."&order=desc";
 
-		echo '<div class="input-group-lg mb-3">';
-  		echo '<input id="linkgit" type="text" class="form-control input-lg">';
-  		echo '<div class="input-group-append">';
-    	echo '<button class="btn btn-outline-secondary" data-clipboard-target="#linkgit" type="button">Button</button></div></div>';
-
-    	//echo $prueba;
+    	echo '<div class="input-group">';
+    	echo '<input id="linkgit" type="text" value="'.$urlClipboard.'" class="form-control" style="font-size: 16px;">';
+		echo '<span class="input-group-btn">';
+		echo '<button id="copia" class="btn btn-default" data-clipboard-target="#linkgit" type="button">Copiar</button>';
+		echo '</span>';
+		echo '</div>';
 
     	$result = wp_remote_get( $url ); //Consumiendo la API mediante GET
     	$result = json_decode($result['body'], true); //Transformando la data a lo que nos interesa
@@ -69,20 +73,16 @@
 	    if(get_option('estadoghf')==1) return "Disculpe, estamos mejorando el plugin.";
     	else return $dataSalida;
 	}
-
-	function githubFinder_disabled($atts){
-		return "Disculpe, estamos mejorando el plugin.";
-	}
-
 	add_shortcode("github-finder", "githubFinder");
 
-	function crear_menu() {
+	//Menu para poner al plugin en estado de matenimiento
+	function menu_gitHubFinder() {
   	add_menu_page('Github finder', 'Github finder', 'manage_options', 'ghf_mantenimiento', 'output_menu');
 	  function output_menu() {
 	  echo "<form action=''>";
 	  echo "<h1>Activar/Desactivar funcionalidad del GitHub finder</h1>";
 	  echo "<p>Esta funcionalidad nos permite desactivar temporalmente el plugin de github.</p>";
-	  if(get_option('estadoghf')==0){
+	  if(get_option('estadoghf')==0 || get_option('estadoghf')==false){
 	  	echo "<input type='hidden' name='mantenimiento' value='1'>";
 	  	echo "<input type='checkbox' name='estado' value='1' unchecked onChange='this.form.submit()''>Modo mantenimiento</input>";
 	  }
@@ -106,6 +106,49 @@
 	  	  header('Location: ../wp-admin');
 	  }
 	}
-	add_action("admin_menu", "crear_menu");
+	add_action("admin_menu", "menu_gitHubFinder");
+
+	/* Criterio de busqueda para github */
+
+	function criterio_post_type(){
+		//definiendo los nombres de las etiquetas de nombre
+		$labels = array(
+		'name'                  => __( 'Criterios de búsqueda', 'testtheme' ),
+		'singular_name'         => __( 'criterios', 'testtheme' ),
+		'menu_name'             => __( 'Criterio', 'testtheme' ),
+		'name_admin_bar'        => __( 'Criterio', 'testtheme' ),
+		'archives'              => __( 'Archivos de criterios', 'testtheme' ),
+		'all_items'             => __( 'Todos los criterios', 'testtheme' ),
+		'add_new_item'          => __( 'Agregar nuevo criterio', 'testtheme' ),
+		'add_new'               => __( 'Agregar criterio', 'testtheme' ),
+		'new_item'              => __( 'Nuevo criterio', 'testtheme' ),
+		'edit_item'             => __( 'Editar criterio', 'testtheme' ),
+		'update_item'           => __( 'Actualizar criterio', 'testtheme' ),
+		'view_item'             => __( 'Ver criterio', 'testtheme' ),
+		'view_items'            => __( 'Ver criterio', 'testtheme' ),
+		'search_items'          => __( 'Buscar criterios', 'testtheme' ),
+		'not_found'             => __( 'No encontrado', 'testtheme' ),
+		'featured_image'        => __( 'Imagen Destacada', 'testtheme' ),
+		'set_featured_image'    => __( 'Colocar imagen destacada', 'testtheme' ),
+		'remove_featured_image' => __( 'Quitar imagen destacada', 'testtheme' ),
+		'use_featured_image'    => __( 'Usar como imagen destacada', 'testtheme' ),
+		'insert_into_item'      => __( 'Insertar en el criterio', 'testtheme' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'testtheme' ),
+		'items_list'            => __( 'Lista de criterios', 'testtheme' )
+		);
+
+	$args = array(
+		'label'                 => __( 'Criterio', 'juanfederico' ),
+		'description'           => __( 'Lista de criterios', 'juanfederico' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'thumbnail','editor' ),
+		'taxonomies'            => array( 'Dominios','Hosting' ),
+		'public'                => true,
+		'menu_position'         => 5,
+		'has_archive'           => 'criterios'
+	);
+	register_post_type( 'criterio', $args );
+	}
+	add_action('init', 'criterio_post_type');
 
 ?>
--- wp-content/plugins/github-finder/js/script.js
+++ wp-content/plugins/github-finder/js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
     var clipboard = new ClipboardJS('.btn');
 });
-
-$('#linkgit').keypress(function(e){
-	alert("apreto");
-	return false;
-});
